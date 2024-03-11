68cba79a0a711d8b1bdcefb3f4474b3b13eabacb
MoazMohamedHamzaGaber <moaz68860@gmail.com>
2024-03-11 17:55:06 +0200

    custom loading

---
+++ lib/core/utiles/custom_Loading.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class CustomLoading extends StatelessWidget {
+  const CustomLoading({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(child: CircularProgressIndicator());
+  }
+}
