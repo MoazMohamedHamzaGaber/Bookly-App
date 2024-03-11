da4ca6d11e3a4172648277f7c2a0acf69a8dca5a
MoazMohamedHamzaGaber <moaz68860@gmail.com>
2024-03-11 16:34:26 +0200

    Api Service

---
+++ lib/core/utiles/api_service.dart
@@ -0,0 +1,16 @@
+import 'package:dio/dio.dart';
+
+class ApiService{
+  final String _baseUrl='https://www.googleapis.com/books/v1';
+
+  final Dio dio;
+
+  ApiService({required this.dio});
+
+  Future<Response> get({
+    required String url,
+})async{
+  var response= await dio.get('$_baseUrl$url');
+  return response.data;
+  }
+}
