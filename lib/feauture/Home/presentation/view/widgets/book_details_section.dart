2b0096846e7868561e74a6be53154cad7750c99b
MoazMohamedHamzaGaber <moaz68860@gmail.com>
2024-03-12 17:30:28 +0200

    Book details

--- lib/feauture/Home/presentation/view/widgets/book_details_section.dart
+++ lib/feauture/Home/presentation/view/widgets/book_details_section.dart
@@ -1,3 +1,5 @@
+import 'package:bookly/feauture/Home/data/models/book_model/items.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 
 import '../../../../../core/utiles/style.dart';
@@ -5,8 +7,9 @@ import 'book_action_botton.dart';
 import 'book_rating.dart';
 
 class BookDetailsSection extends StatelessWidget {
-   const BookDetailsSection({Key? key}) : super(key: key);
+   const BookDetailsSection({Key? key, required this.items}) : super(key: key);
 
+   final Items items;
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -20,19 +23,25 @@ class BookDetailsSection extends StatelessWidget {
             height: 245,
             decoration: BoxDecoration(
                 borderRadius: BorderRadius.circular(16),
-                image: const DecorationImage(
-                    fit: BoxFit.fill,
-                    image: NetworkImage('https://letsenhance.io/static/8f5e523ee6b2479e26ecc91b9c25261e/1015f/MainAfter.jpg'))),
+            ),
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(16),
+              child: CachedNetworkImage(
+                fit: BoxFit.fill,
+                imageUrl: items.volumeInfo.imageLinks!.thumbnail,
+                errorWidget: (context, url, error) => const Icon(Icons.error_outline),
+              ),
+            ),
           ),
         ),
-         const Text('Digital Image Processing',
+          Text(items.volumeInfo.title!,
           textAlign: TextAlign.center,
           style: Style.textStyle30,
         ),
         const SizedBox(
           height: 4,
         ),
-        Text('items.volumeInfo!.authors![0]',
+        Text(items.volumeInfo.authors![0],
           style: Style.textStyle18.copyWith(
             fontWeight: FontWeight.w500,
             color: const Color(0xff707070),
@@ -41,7 +50,10 @@ class BookDetailsSection extends StatelessWidget {
         const SizedBox(
           height: 14,
         ),
-          const BookRating(rating: 10,count: 4),
+        BookRating(
+          rating: items.volumeInfo.averageRating ??0,
+          count: items.volumeInfo.ratingsCount ??0,
+        ),
          const BookActionBottom(),
       ],
     );
