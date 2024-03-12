2d933e9068ff2a8096ed87f00c8b65fd09f63717
MoazMohamedHamzaGaber <moaz68860@gmail.com>
2024-03-12 17:30:38 +0200

    Book details view

--- lib/feauture/Home/presentation/view/book_details_view.dart
+++ lib/feauture/Home/presentation/view/book_details_view.dart
@@ -1,13 +1,28 @@
 import 'package:bookly/feauture/Home/presentation/view/widgets/book_details_view_body.dart';
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../../../../core/utiles/api_service.dart';
+import '../../data/models/book_model/items.dart';
+import '../../data/repository/home_repo_impl.dart';
+import '../manage/similar_books_cubit/similar_books_cubit.dart';
 
 class BookDetailsView extends StatelessWidget {
-   const BookDetailsView({Key? key}) : super(key: key);
+   const BookDetailsView({Key? key, required this.items}) : super(key: key);
 
+   final Items items;
   @override
   Widget build(BuildContext context) {
-    return  const Scaffold(
-      body: BookDetailsViewBody(),
+    return  BlocProvider(
+      create: (BuildContext context) => SimilarBooksCubit(
+        HomeReposImpl(
+          ApiService(Dio()),
+        ),
+      )..fetchSimilarBooks(category: items.volumeInfo.categories![0]),
+      child: Scaffold(
+        body: BookDetailsViewBody(items: items,),
+      ),
     );
   }
 }
