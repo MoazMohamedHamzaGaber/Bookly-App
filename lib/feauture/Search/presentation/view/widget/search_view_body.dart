5ebf7e66cf886b4efcf4f2cb1cad1d55fafc7232
MoazMohamedHamzaGaber <moaz68860@gmail.com>
2024-03-14 02:55:07 +0200

    Search view body

--- lib/feauture/Search/presentation/view/widget/search_view_body.dart
+++ lib/feauture/Search/presentation/view/widget/search_view_body.dart
@@ -1,51 +1,39 @@
+import 'package:bookly/core/utiles/setup_service_locator.dart';
+import 'package:bookly/feauture/Search/data/repository/search_repo_impl.dart';
+import 'package:bookly/feauture/Search/presentation/manage/cubit/search_cubit.dart';
+import 'package:bookly/feauture/Search/presentation/manage/cubit/search_states.dart';
+import 'package:bookly/feauture/Search/presentation/view/widget/search_app_bar.dart';
+import 'package:bookly/feauture/Search/presentation/view/widget/search_text_field.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'list_view.dart';
 
 class SearchViewBody extends StatelessWidget {
-   const SearchViewBody({Key? key}) : super(key: key);
+  const SearchViewBody({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-        child: SingleChildScrollView(
-          physics: const BouncingScrollPhysics(),
-          child: Padding(
-            padding: const EdgeInsets.all(20.0),
-            child: Column(
-              children: [
-                TextFormField(
-                  onFieldSubmitted: (value)
-                  {
-                  //  cubit.getSearch(value);
-                  },
-                  onChanged: (value)
-                  {
-                 //   cubit.getSearch(value);
-                  },
-                  decoration: InputDecoration(
-                      hintText: 'Search',
-                      enabledBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(20),
-                        borderSide: const BorderSide(
-                            color: Colors.white
-                        ),
-                      ),
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(20),
-                      ),
-                      suffixIcon: const Icon(Icons.search)
+    return BlocBuilder<SearchCubit, SearchStates>(
+      builder: (BuildContext context, state) {
+        var cubit = SearchCubit(getIt.get<SearchRepoImpl>()).get(context);
+        return Scaffold(
+          appBar: buildAppBar(context),
+          body: SafeArea(
+            child: Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Column(
+                children: [
+                  SearchTextField(cubit: cubit,),
+                  const SizedBox(
+                    height: 20,
                   ),
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-                const ListViews(),
-              ],
+                  const ListViews(),
+                ],
+              ),
             ),
           ),
-        ),
-      ),
+        );
+      },
     );
   }
 }
