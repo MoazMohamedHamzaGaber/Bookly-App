d325ea781017850b66bef42bc95fdaf20d563576
MoazMohamedHamzaGaber <moaz68860@gmail.com>
2024-03-15 22:58:50 +0200

    Delete bottom cart from app bar in details book

--- lib/feauture/Home/presentation/view/widgets/custom_book_details_app_bar.dart
+++ lib/feauture/Home/presentation/view/widgets/custom_book_details_app_bar.dart
@@ -5,28 +5,15 @@ class CustomBookDetailsAppBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: [
-        IconButton(
-          onPressed: () {
-            Navigator.pop(context);
-          },
-          icon: const Icon(
-            Icons.close_outlined,
-            color: Colors.white,
-            size: 30,
-          ),
-        ),
-        const Spacer(),
-        IconButton(
-          onPressed: () {},
-          icon: const Icon(
-            Icons.shopping_cart_outlined,
-            color: Colors.white,
-            size: 25,
-          ),
-        ),
-      ],
+    return IconButton(
+      onPressed: () {
+        Navigator.pop(context);
+      },
+      icon: const Icon(
+        Icons.close_outlined,
+        color: Colors.white,
+        size: 30,
+      ),
     );
   }
 }
