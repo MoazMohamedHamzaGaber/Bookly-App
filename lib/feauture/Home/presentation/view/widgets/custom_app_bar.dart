559a37ff965532d7b9a37386c898e056b32b8426
MoazMohamedHamzaGaber <moaz68860@gmail.com>
2024-03-15 22:59:20 +0200

    Build appbar to home view

--- lib/feauture/Home/presentation/view/widgets/custom_app_bar.dart
+++ lib/feauture/Home/presentation/view/widgets/custom_app_bar.dart
@@ -2,30 +2,34 @@ import 'package:bookly/feauture/Search/presentation/view/search_view.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
-class CustomAppBar extends StatelessWidget {
-  const CustomAppBar({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Row(
-      children: [
-        const Image(
-          width: 75,
-          height: 16,
-          image: AssetImage('assets/images/Logo.png'),
-        ),
-        const Spacer(),
+PreferredSizeWidget buildAppBar(context) => AppBar(
+      backgroundColor: const Color(0xff100B20),
+      titleSpacing: 20,
+      elevation: 0,
+      title: const Image(
+        width: 75,
+        height: 16,
+        image: AssetImage('assets/images/Logo.png'),
+      ),
+      actions: [
         IconButton(
           onPressed: () {
-            Navigator.push(context, MaterialPageRoute(builder: (context)=>SearchView(),),);
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => SearchView(),
+              ),
+            );
           },
           icon: const Icon(
             FontAwesomeIcons.magnifyingGlass,
             color: Colors.white,
             size: 22,
           ),
-        )
+        ),
+        const SizedBox(
+          width: 10,
+        ),
       ],
     );
-  }
-}
+
