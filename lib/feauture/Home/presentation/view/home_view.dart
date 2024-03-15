423be3f7ee87b35bab82dfb63e2f5fec8d05741d
MoazMohamedHamzaGaber <moaz68860@gmail.com>
2024-03-15 23:44:52 +0200

    Add willPopScope to home view

--- lib/feauture/Home/presentation/view/home_view.dart
+++ lib/feauture/Home/presentation/view/home_view.dart
@@ -2,14 +2,19 @@ import 'package:bookly/feauture/Home/presentation/view/widgets/custom_app_bar.da
 import 'package:bookly/feauture/Home/presentation/view/widgets/home_view_body.dart';
 import 'package:flutter/material.dart';
 
+import '../../../../core/utiles/constants.dart';
+
 class HomeView extends StatelessWidget {
   const HomeView({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-      appBar: buildAppBar(context),
-      body: const HomeViewBody(),
+    return  WillPopScope(
+      onWillPop:()=> buildShowDialog(context),
+      child: Scaffold(
+        appBar: buildAppBar(context),
+        body: const HomeViewBody(),
+      ),
     );
   }
 }
