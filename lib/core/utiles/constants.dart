1b3e95d18ad8d0408618e0d919d5006bfdb66468
MoazMohamedHamzaGaber <moaz68860@gmail.com>
2024-03-15 23:46:02 +0200

    Build showDialog

--- lib/core/utiles/constants.dart
+++ lib/core/utiles/constants.dart
@@ -1,3 +1,4 @@
+import 'package:bookly/core/utiles/style.dart';
 import 'package:flutter/material.dart';
 
 const kPrimaryColor = Color(0xff100B20);
@@ -22,5 +23,49 @@ Widget buildMaterialApp({
     ),
   ),
 );
+
+Future<bool> buildShowDialog(context) async{
+  bool exitApp=await showDialog(
+    context: context,
+    builder: (context) =>
+        AlertDialog(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(12),
+          ),
+          backgroundColor: Theme
+              .of(context)
+              .scaffoldBackgroundColor,
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              const Text('Do you want to close the app?',
+              style: Style.textStyle16,
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  TextButton(
+                    onPressed: () {
+                      Navigator.of(context).pop(false);
+                    },
+                    child: const Text('Cancel'),
+                  ),
+                  TextButton(
+                    onPressed: () {
+                      Navigator.of(context).pop(true);
+                    },
+                    child: const Text('OK'),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+  );
+  return exitApp ??false;
+}
 
 var searchController=TextEditingController();
